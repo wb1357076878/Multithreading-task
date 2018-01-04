bdee0a60efe169c3b056beb777dfc6f0c8dffeb6
wangbo <1357076878@qq.com>
2018-01-04 23:34:03 +0800

    simple Demo

---
+++ Cassini/Cassini/DemoURL.swift
@@ -0,0 +1,13 @@
+//
+//  DemoURL.swift
+//  Cassini
+//
+//  Created by WangBo on 2018/1/4.
+//  Copyright © 2018年 WangBo. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURL {
+    static let stanford = URL(string: "https://www-media.stanford.edu/wp-content/uploads/2017/03/24182714/about_landing-1.jpg")
+}
---
+++ Cassini/Cassini/ImageViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by WangBo on 2018/1/4.
+//  Copyright © 2018年 WangBo. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController
+{
+    var imageURL: URL? {
+        didSet {
+            image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    // 这部操作是耗时的
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageURL = DemoURL.stanford
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if image == nil {
+            fetchImage()
+        }
+    }
+    
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.delegate = self
+            scrollView.minimumZoomScale = 0.03
+            scrollView.maximumZoomScale = 1.0
+            scrollView.contentSize = imageView.frame.size
+            scrollView.addSubview(imageView)
+        }
+    }
+    
+    fileprivate var imageView = UIImageView()
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView?.contentSize = imageView.frame.size
+        }
+    }
+}
+
+extension ImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+}
